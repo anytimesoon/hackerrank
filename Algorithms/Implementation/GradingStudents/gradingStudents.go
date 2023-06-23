9b8a0108dba88535a672a259f69fdb85c0499eda
Damien de Lartigue <damien@delartigue.co.uk>
2023-06-23 17:43:27 +0100

    improved solution to Grading Students

--- Algorithms/Implementation/GradingStudents/gradingStudents.go
+++ Algorithms/Implementation/GradingStudents/gradingStudents.go
@@ -1,28 +1,27 @@
 package GradingStudents
 
-import "strconv"
-
 func gradingStudents(grades []int32) []int32 {
 	res := make([]int32, 0)
-	m := map[string]bool{
-		"3": true,
-		"4": true,
-		"8": true,
-		"9": true,
-	}
 
 	for _, grade := range grades {
-		iString := strconv.Itoa(int(grade))
-		if grade < 38 || !m[iString[1:]] {
+		if grade < 38 ||
+			grade%5 == 0 ||
+			(grade-1)%5 == 0 ||
+			(grade-2)%5 == 0 {
 			res = append(res, grade)
 			continue
 		}
 
-		if iString[1:] == "3" || iString[1:] == "8" {
-			res = append(res, grade+2)
-		} else {
-			res = append(res, grade+1)
-		}
+		roundedGrade := roundUp(grade + 1)
+		res = append(res, roundedGrade)
 	}
 	return res
+}
+
+func roundUp(grade int32) int32 {
+	if grade%5 == 0 {
+		return grade
+	}
+
+	return roundUp(grade + 1)
 }
