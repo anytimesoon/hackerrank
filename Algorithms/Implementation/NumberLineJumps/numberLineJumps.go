ca0fea2fe9ed6e373e6e1d44fcf488f6501bc857
Damien de Lartigue <damien@delartigue.co.uk>
2023-06-29 09:42:40 +0100

    add more efficient solution to number line jumps

--- Algorithms/Implementation/NumberLineJumps/numberLineJumps.go
+++ Algorithms/Implementation/NumberLineJumps/numberLineJumps.go
@@ -1,23 +1,9 @@
 package NumberLineJumps
 
 func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
-	if (x1 > x2 && v1 > v2) || (x1 < x2 && v1 < v2) {
+	if v1 <= v2 || (x2-x1)%(v1-v2) != 0 {
 		return "NO"
+	} else {
+		return "YES"
 	}
-
-	if x1 == x2 && v1 != v2 {
-		return "NO"
-	}
-
-	var i int
-	for i < 10000 {
-		if x1 == x2 {
-			return "YES"
-		}
-
-		x1 = x1 + v1
-		x2 = x2 + v2
-		i++
-	}
-	return "NO"
 }
